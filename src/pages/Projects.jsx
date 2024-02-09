4bb8f30c6ca73c4291f352453e1a7aa8f3eae862
Simona <simonachess@gmail.com>
2024-02-09 20:17:29 +0200

    styled comming soon

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-      {/* <h1>Projects</h1> */}
-      <div className='fixed top-[10%] left-[10%]'>Comming soon...</div>
+    <div
+      class="w-full h-screen bg-gradient-to-bl from-teal-400 to-blue-500 flex flex-col justify-center items-center text-white">
+      <h1 class="text-5xl">I am <b>Almost</b> there!</h1>
+      <p>Stay tuned for something amazing!!!</p>
+      <br />
+      <p>Comming soon...</p>
     </div>
   )
 }
